38f814286fb0087a497d97d5f33f1fa450913f47
Eul0gy <7442787+Eul0gy@users.noreply.github.com>
2018-03-09 18:39:40 +0000

    https://github.com/Eul0gy/PingTool.git

---
+++ Pingtool/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Pingtool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
